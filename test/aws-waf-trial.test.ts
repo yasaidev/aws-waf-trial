14ed5b04521b879c39851778ddfdf29586516fec
yasaidev <3955027+yasaidev@users.noreply.github.com>
2024-06-23 14:00:00 +0900

    Initial commit

---
+++ test/aws-waf-trial.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsWafTrial from '../lib/aws-waf-trial-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-waf-trial-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsWafTrial.AwsWafTrialStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
